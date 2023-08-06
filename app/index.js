e8f129a4d28e707fecaedf3681c0177959502b5a
Matt Polky <mdpolky+github@gmail.com>
2023-08-06 14:35:40 -0500

    added remove note action

--- app/index.js
+++ app/index.js
@@ -1,27 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { useState, useReducer } from "react";
+import { useReducer, useState } from "react";
 import {
-  Button,
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from "react-native";
+import { v4 as uuidv4 } from "uuid";
 
-function Note() {
-  const [noteText, onChangeNoteText] = useState(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus et netus et. Sit amet luctus venenatis lectus magna fringilla urna."
-  );
+function Note({ data, dispatch }) {
+  const [noteText, onChangeNoteText] = useState(data.text);
+  const [noteDate, onChangeNoteDate] = useState(data.date); //TODO: onChangeNoteText should change NoteDate as well
   const [isEditable, setIsEditable] = useReducer(
     (editable) => !editable,
     false
   );
+
   return (
     <View style={styles.noteContainer}>
       {isEditable ? (
         <TextInput
-          style={styles.noteText}
+          style={[styles.noteText, styles.noteTextInput]}
           multiline={true}
           onChangeText={onChangeNoteText}
           value={noteText}
@@ -29,32 +30,87 @@ function Note() {
           {noteText}
         </TextInput>
       ) : (
-        <Text style={styles.noteText}>{noteText}</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.noteText}>{noteText}</Text>
+        </ScrollView>
       )}
 
       <View style={styles.noteFooter}>
-        <Text style={styles.noteDate}>August 5, 2023</Text>
-
+        <Text style={styles.noteDate}>{noteDate}</Text>
         <Pressable
-          style={styles.editNoteButton}
+          style={styles.noteDeleteButton}
+          onPress={() => {
+            dispatch({ type: "removed_note", id: data.id });
+          }}
+        >
+          <Text>Remove</Text>
+        </Pressable>
+        <Pressable
+          style={styles.noteEditButton}
           onPress={() => {
             setIsEditable(true);
           }}
         >
-          <Text>Edit</Text>
+          <Text>{isEditable ? "Save" : "Edit"}</Text>
         </Pressable>
       </View>
     </View>
   );
 }
 
+function notesReducer(state, action) {
+  switch (action.type) {
+    case "added_note": {
+      return [{ id: uuidv4(), date: "2023-08-06", text: "" }, ...state];
+    }
+    case "removed_note": {
+      return state.filter((note) => note.id !== action.id);
+    }
+    default:
+      throw Error("Unknown action: " + action.type);
+  }
+}
+const initialNotes = [
+  {
+    id: "a4f928b1-bfbf-4db4-96e3-86565198d1d0",
+    date: "2023-08-05",
+    text: "first note",
+  },
+  {
+    id: "2d7124fb-9713-4365-859d-947dd435a5c9",
+    date: "2023-08-05",
+    text: "second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second notesecond note",
+  },
+  {
+    id: "b0134106-c106-447d-b7d7-5cf116c9d6b9",
+    date: "2023-08-03",
+    text: "3rd note",
+  },
+  {
+    id: "23bc2661-3d71-4b40-abfd-c8d740ad2dc0",
+    date: "2023-08-02",
+    text: "4th note",
+  },
+  {
+    id: "595f7dba-74a8-4108-9800-3c3f9216268f",
+    date: "2023-08-01",
+    text: "fifth note",
+  },
+];
+
 export default function App() {
   const [searchText, onChangeSearchText] = useState("");
+  const [notes, notesDispatch] = useReducer(notesReducer, initialNotes);
   return (
     <View style={styles.container}>
       <View style={styles.leftRail}>
         <Text style={styles.logo}>Post-ems</Text>
-        <Pressable style={styles.addNoteButton}>
+        <Pressable
+          style={styles.addNoteButton}
+          onPress={() => {
+            notesDispatch({ type: "added_note" });
+          }}
+        >
           <Text>Add Note</Text>
         </Pressable>
       </View>
@@ -67,18 +123,20 @@ export default function App() {
         />
         <Text style={styles.noteTitle}>Notes</Text>
         <View style={styles.notesContainer}>
-          <Note />
-          <Note />
-          <Note />
-          <Note />
-          <Note />
-          <Note />
+          {notes.map((note) => {
+            return <Note key={note.id} data={note} dispatch={notesDispatch} />;
+          })}
         </View>
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+//noteTextHeight+noteFooterHeight should add up to noteSize
+const noteSize = "300px";
+const noteTextHeight = "250px";
+const noteFooterHeight = "50px";
 
 const styles = StyleSheet.create({
   container: {
@@ -110,22 +168,32 @@ const styles = StyleSheet.create({
   noteTitle: { fontSize: 36, fontWeight: 600 },
   noteContainer: {
     flex: 1,
+    flexShrink: 1,
     padding: 10,
     marginTop: 20,
     borderWidth: "1px",
-    minWidth: "300px",
+    minHeight: noteSize,
+    maxHeight: noteSize,
+    minWidth: noteSize,
+    maxWidth: noteSize,
     backgroundColor: "blanchedalmond",
     borderColor: "blanchedalmond",
     borderRadius: 10,
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
   },
   noteText: {
     fontSize: 20,
   },
+  noteTextInput: {
+    height: noteTextHeight,
+  },
   noteFooter: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    height: noteFooterHeight,
   },
   noteDate: {},
+  noteDeleteButton: {},
   noteEditButton: {},
 });
