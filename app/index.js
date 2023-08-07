f4b2363e8867ae36ddb70a9c6304ef27cbc36217
Matt Polky <mdpolky+github@gmail.com>
2023-08-07 14:55:26 -0500

    added basic animation for notes, added colors and fab

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useReducer, useState } from "react";
 import {
+  Easing,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -10,17 +11,38 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { v4 as uuidv4 } from "uuid";
+import { AnimatePresence, Motion } from "@legendapp/motion";
 
 function Note({ data, dispatch }) {
   const [noteText, onChangeNoteText] = useState(data.text);
-  const [noteDate, onChangeNoteDate] = useState(data.date); //TODO: onChangeNoteText should change NoteDate as well
+  const [noteDate, setNoteDate] = useState(data.date); //TODO: onChangeNoteText should change NoteDate as well
   const [isEditable, setIsEditable] = useReducer(
     (editable) => !editable,
     false
   );
-
+  const noteContainerStyle = data.color
+    ? [styles.noteContainer, { backgroundColor: data.color }]
+    : styles.noteContainer;
   return (
-    <View style={styles.noteContainer}>
+    <Motion.View
+      key={data.id}
+      style={noteContainerStyle}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{
+        default: {
+          type: "spring",
+          damping: 20,
+          stiffness: 300,
+        },
+        opacity: {
+          type: "tween",
+          duration: 300,
+        },
+      }}
+      layout={{ duration: 2.0 }}
+    >
       {isEditable ? (
         <TextInput
           style={[styles.noteText, styles.noteTextInput]}
@@ -56,17 +78,25 @@ function Note({ data, dispatch }) {
             dispatch({ type: "removed_note", id: data.id });
           }}
         >
-          <AntDesign name="close" size={24} color="black" />
+          <AntDesign
+            style={styles.noteDeleteIcon}
+            name="close"
+            size={24}
+            color="black"
+          />
         </Pressable>
       </View>
-    </View>
+    </Motion.View>
   );
 }
 
 function notesReducer(state, action) {
   switch (action.type) {
     case "added_note": {
-      return [{ id: uuidv4(), date: "2023-08-06", text: "" }, ...state];
+      return [
+        { id: uuidv4(), date: "2023-08-06", text: "", color: action.color },
+        ...state,
+      ];
     }
     case "removed_note": {
       return state.filter((note) => note.id !== action.id);
@@ -80,50 +110,92 @@ const initialNotes = [
     id: "a4f928b1-bfbf-4db4-96e3-86565198d1d0",
     date: "2023-08-05",
     text: "first note",
+    color: "tomato",
   },
   {
     id: "2d7124fb-9713-4365-859d-947dd435a5c9",
     date: "2023-08-05",
     text: "second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second note second notesecond note",
+    color: "peachpuff",
   },
   {
     id: "b0134106-c106-447d-b7d7-5cf116c9d6b9",
     date: "2023-08-03",
     text: "3rd note",
+    color: "blanchedalmond",
   },
   {
     id: "23bc2661-3d71-4b40-abfd-c8d740ad2dc0",
     date: "2023-08-02",
     text: "4th note",
+    color: "yellowgreen",
   },
   {
     id: "595f7dba-74a8-4108-9800-3c3f9216268f",
     date: "2023-08-01",
     text: "fifth note",
+    color: "powderblue",
+  },
+  {
+    id: "2b5141b9-cf2d-4346-b3cc-60165e48d352",
+    date: "2023-08-01",
+    text: "This is the sixth thing",
+    color: "rebeccapurple",
   },
 ];
+
+function FabAction({ color, dispatch }) {
+  return (
+    <Pressable
+      style={{ marginTop: 25 }}
+      onPress={() => {
+        dispatch({ type: "added_note", color: color });
+      }}
+    >
+      <View style={[styles.fabItem, { backgroundColor: color }]}></View>
+    </Pressable>
+  );
+}
 
 export default function App() {
   const [searchText, onChangeSearchText] = useState("");
   const [notes, notesDispatch] = useReducer(notesReducer, initialNotes);
-
+  const [isShownFab, setIsShownFab] = useReducer((hidden) => !hidden, false);
+  const fabActions = [
+    "tomato",
+    "peachpuff",
+    "blanchedalmond",
+    "yellowgreen",
+    "powderblue",
+    "rebeccapurple",
+  ];
   return (
     <View style={styles.container}>
       <View style={styles.leftRail}>
         <Text style={styles.logo}>Post-ems</Text>
-        <Pressable
-          style={styles.addNoteButton}
-          onPress={() => {
-            notesDispatch({ type: "added_note" });
-          }}
-        >
-          <AntDesign
-            style={styles.addNoteButtonIcon}
-            name="pluscircle"
-            size={32}
-            color="green"
-          />
-        </Pressable>
+        <View style={styles.fabContainer}>
+          <Pressable style={styles.addNoteButton} onPress={setIsShownFab}>
+            <AntDesign
+              style={styles.addNoteButtonIcon}
+              name="pluscircle"
+              size={32}
+              color="slategray"
+            />
+          </Pressable>
+          {isShownFab && (
+            <View style={styles.fabItems}>
+              {fabActions.map((color) => {
+                return (
+                  <FabAction
+                    key={color}
+                    color={color}
+                    dispatch={notesDispatch}
+                  />
+                );
+              })}
+            </View>
+          )}
+        </View>
       </View>
       <View style={styles.mainContainer}>
         <TextInput
@@ -133,14 +205,17 @@ export default function App() {
           value={searchText}
         />
         <Text style={styles.noteTitle}>Notes</Text>
+
         <View style={styles.notesContainer}>
-          {notes
-            .filter((note) => note.text.includes(searchText))
-            .map((note) => {
-              return (
-                <Note key={note.id} data={note} dispatch={notesDispatch} />
-              );
-            })}
+          <AnimatePresence>
+            {notes
+              .filter((note) => note.text.includes(searchText))
+              .map((note) => {
+                return (
+                  <Note key={note.id} data={note} dispatch={notesDispatch} />
+                );
+              })}
+          </AnimatePresence>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -167,8 +242,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   logo: { marginBottom: 20, fontWeight: 700 },
+  fabContainer: { alignItems: "center" },
   addNoteButton: {},
   addNoteButtonIcon: {},
+  fabItems: {},
+  fabItem: { borderRadius: "50%", height: 25, width: 25 },
   mainContainer: {
     flex: 9,
     padding: 10,
@@ -179,14 +257,14 @@ const styles = StyleSheet.create({
   notesContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
-    gap: 20,
   },
-  noteTitle: { fontSize: 36, fontWeight: 600 },
+  noteTitle: { fontSize: 36, fontWeight: 600, marginBottom: 20 },
   noteContainer: {
     flex: 1,
     flexShrink: 1,
     padding: 10,
-    marginTop: 20,
+    marginRight: 20,
+    marginBottom: 20,
     borderWidth: "1px",
     minHeight: noteSize,
     maxHeight: noteSize,
@@ -211,5 +289,6 @@ const styles = StyleSheet.create({
   },
   noteDate: {},
   noteDeleteButton: {},
+  noteDeleteIcon: {},
   noteEditButton: {},
 });
